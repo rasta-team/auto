a28b73d96c80738794a2180df0c1a57cd6459171
rasta-team <irfan1037.psa@gmail.com>
2021-05-18 00:02:56 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
 echo "Checking VPS"
-IZIN=$( curl http://vip-sg1.myvpskuy.xyz:81/BskaoahdmsoahamoaJNlapabsmal | grep $MYIP )
+IZIN=$( curl https://unlimitedteam.tech/script/ipvps | grep $MYIP )
 if [ $MYIP = $IZIN ]; then
 echo -e "${green}Permission Accepted...${NC}"
 else
